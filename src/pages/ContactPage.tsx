dc4ea83ab70530684ececc8026e4703209d5c43d
Deivis888 <deivis777@gmail.com>
2025-05-18 16:35:36 +0300

    Fix reCAPTCHA cleanup function

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -14,7 +14,7 @@ declare global {
       reset: (id?: number) => void;
       getResponse: (id?: number) => string;
     };
-    onRecaptchaLoad: () => void;
+    onRecaptchaLoad?: () => void;
   }
 }
 
@@ -100,8 +100,8 @@ const ContactPage = () => {
     };
 
     return () => {
-      if ('onRecaptchaLoad' in window) {
-        delete window.onRecaptchaLoad;
+      if (typeof window.onRecaptchaLoad !== 'undefined') {
+        window.onRecaptchaLoad = undefined;
       }
     };
   }, []);
