2630c8c823d2c74f6bc69ea07e7a6421884e34d6
Deivis888 <deivis777@gmail.com>
2025-06-08 14:41:56 +0300

    Remove duplicate employee button from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -216,12 +216,6 @@ const Header = () => {
               >
                 {t('navigation.register')}
               </Link>
-              <Link
-                to="/employee/login"
-                className="btn btn-secondary btn-sm"
-              >
-                Darbuotojai
-              </Link>
             </div>
           </div>
 
@@ -359,12 +353,6 @@ const Header = () => {
                 className="btn btn-primary btn-md justify-start"
               >
                 {t('navigation.register')}
-              </Link>
-              <Link
-                to="/employee/login"
-                className="btn btn-secondary btn-md justify-start"
-              >
-                Darbuotojai
               </Link>
             </div>
           </div>
